7c479f2e80f82af0c8b6b449deb8b1535eda4ec2
Skay Hall <shanakayhall@gmail.com>
2020-04-19 17:25:25 -0400

    stats - getting info dynamically from API

--- statsscript.js
+++ statsscript.js
@@ -1,50 +1,37 @@
-/*
-var data = $.ajax({
-    type: "GET",
-    url: "https://api.smartable.ai/coronavirus/stats/CA-ON",
-
-    // Request headers
-    beforeSend: function(xhrObj) {
-        xhrObj.setRequestHeader("Subscription-Key", "fa4cb3e91fa04405b764206734f8bfa6");
-        },
-    })
-.done(function (data) {
-    alert("success");
-})
-.fail(function () {
-    alert("error");
-});
-
-console.log(data);
-
-*/
-
-
-
-
 $.getJSON("https://cors-anywhere.herokuapp.com/https://data.ontario.ca/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11", function(data) {
     console.log(data);
 
-    //var weathericon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    //console.log(weathericon);
 
-    var statscon = data.result.records[72].Deaths;
-    console.log(statscon);
 
-    var statsreso = data.result.records[72].Resolved;
+    var laststats = data.result.records.length;
+    //var statstot =  data.result.laststats.Deaths;
+    //var lastitem = laststats[laststats.length -1];
+    //console.log("The last item is " + lastitem);
+    console.log("The last item is " + laststats);
 
-    var statstotal = data.result.records[72]["Total Cases"];
 
-    //var weathername = data.weather[0].description;
+    //grabs deaths
+    var statsdead = data.result.records[data.result.records.length-1].Deaths;
+    console.log(statsdead);
+    //var statsdead = data.result.records[72].Deaths;
+    //console.log(statsdead);
 
-    //var locationname = data.name;
+    //grabs resolved cases
+    var statsreso = data.result.records[data.result.records.length-1].Resolved;
+    //var statsreso = data.result.records[72].Resolved;
 
-    //$('.icon').attr('src', weathericon);
-    $('.statsconfirmed').append(statscon);
-    $('.statsresolved').append(statsreso);
-    $('.statstotal').append(statstotal);
+    //grabs total cases
+    var statstotal = data.result.records[data.result.records.length-1]["Total Cases"];
+    //var statstotal = data.result.records[72]["Total Cases"];
 
-    //$('.location').append(locationname);
+    var lastupdated = data.result.records[data.result.records.length-1]["Reported Date"];
+
+
+    $('.stats-dead').append(statsdead);
+    $('.stats-reso').append(statsreso);
+    $('.stats-con').append(statstotal);
+    $('.lastupdated').append(lastupdated);
+
 
 });
 
