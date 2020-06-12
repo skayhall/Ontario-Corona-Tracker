8d5c28a8851f77e0e50785f177028c56c0a631f7
Skay Hall <shanakayhall@gmail.com>
2020-06-12 11:43:02 -0400

    updated getJSON on stats script

--- js/statsscript.js
+++ js/statsscript.js
@@ -1,8 +1,60 @@
 
-  $.getJSON("https://cors-anywhere.herokuapp.com/https://data.ontario.ca/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11", function(data) {
+  $.getJSON("https://cors-anywhere.herokuapp.com/https://data.ontario.ca/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=0", function(data) {
       console.log(data);
 
 
+      //on success get the total days
+      var totaldays = data.result.total;
+      console.log("The total is: " + totaldays); 
+
+
+      //grab the most recent record
+      $.getJSON("https://cors-anywhere.herokuapp.com/https://data.ontario.ca/api/3/action/datastore_search?offset=" + (totaldays-1) + "&resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=1", function(data) {
+        console.log(data);
+
+
+        //grap the about of people who died 
+        var statsdead = data.result.records[0].Deaths;
+        console.log("The dead stats are: " + statsdead);
+
+        //append dead stats to div
+        $('.stats-dead').append(statsdead);
+
+
+        //grab the confirmed cases 
+        var confirmedcases = data.result.records[0]["Total Cases"];
+        console.log("The total cases are: " + confirmedcases);
+
+        //append confirmed cases to div 
+        $('.stats-con').append(confirmedcases);
+
+
+        //grab the resolved cases
+        var resocases = data.result.records[0].Resolved;
+        console.log("The recovered cases are: " + resocases);
+
+        //append confirmed cases to div
+        $('.stats-reso').append(resocases);
+
+
+        //grab the last updated date
+        var lastupdated = data.result.records[0]["Reported Date"];
+        //append last updated info
+        $('.lastupdated').append(lastupdated);
+  
+
+
+      });
+
+
+
+  });
+
+
+
+
+
+/*
 
       //stores the stats
       var laststats = data.result.records.length;
@@ -35,6 +87,4 @@
       $('.stats-con').append(statstotal);
       $('.lastupdated').append(lastupdated);
 
-
-  });
-
+      */
